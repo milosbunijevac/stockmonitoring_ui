d1426974c3d2158050f9534bcf9812859b74cb2d
Milos Bunijevac <milos.bunijevac@gmail.com>
2021-02-28 17:04:41 -0500

    Got textfield hooked up to use redux state

--- src/components/Stocks/Stocks.tsx
+++ src/components/Stocks/Stocks.tsx
@@ -1,10 +1,30 @@
-import React from "react";
-// import Input from "@material-ui/core/Input";
+import React, { ChangeEvent } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import TextField from "@material-ui/core/TextField";
+import { StockTickerNameState } from "../../type";
+import { editTickerNameActionCreator } from "../../redux/store";
 
 const Stocks: React.FC = () => {
+  const dispatch = useDispatch();
+  const tickerValue = useSelector(
+    (state: StockTickerNameState) => state.ticker
+  );
+
+  const handleTickerChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    e.preventDefault();
+    dispatch(editTickerNameActionCreator(e.target.value));
+  };
+
   return (
     <>
-      <div>fdsf</div>
+      <TextField
+        id="stockTickerEntry"
+        label="Stock Ticker"
+        variant="outlined"
+        value={tickerValue}
+        onChange={handleTickerChange}
+        helperText="Enter stock ticker"
+      />
     </>
   );
 };
